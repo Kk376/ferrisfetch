e56707c6a0f9847f811207b2a07a0a50db739ef8
Kushagra Kumar <kkushagra86@gmail.com>
2026-08-16 23:41:11 +0530

    build: sync Termux packaging build.sh with latest release tag sha256

--- packaging/termux/build.sh
+++ packaging/termux/build.sh
@@ -1,19 +1,21 @@
-TERMUX_PKG_HOMEPAGE="https://github.com/Kk376/ferrisfetch"
+TERMUX_PKG_HOMEPAGE=https://github.com/Kk376/ferrisfetch
 TERMUX_PKG_DESCRIPTION="A fast, lightweight Linux and Android system information fetch CLI written in Rust"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Kushagra Kumar <kk376@users.noreply.github.com>"
 TERMUX_PKG_VERSION="0.1.0"
-TERMUX_PKG_SRCURL="https://github.com/Kk376/ferrisfetch/archive/refs/tags/v0.1.0.tar.gz"
-TERMUX_PKG_SHA256="81d8c0651d91ab38b621cc9640194ce8f2ac8fdbbf1a2911f11c784d8326941b"
+TERMUX_PKG_SRCURL="https://github.com/Kk376/ferrisfetch/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=6e2fbf052988cb945c436087f42dcf9ebd8885d3cd19638b58221e7d41eca9df
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
-	cargo build --jobs "$TERMUX_PKG_MAKE_PROCESSES" --target "$CARGO_TARGET_NAME" --release
+	termux_setup_rust
+	cargo build --jobs $TERMUX_PKG_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
 }
 
 termux_step_make_install() {
-	install -Dm755 "target/$CARGO_TARGET_NAME/release/ferrisfetch" "$TERMUX_PREFIX/bin/ferrisfetch"
-	install -Dm644 completions/ferrisfetch.bash "$TERMUX_PREFIX/share/bash-completion/completions/ferrisfetch"
-	install -Dm644 completions/_ferrisfetch "$TERMUX_PREFIX/share/zsh/site-functions/_ferrisfetch"
-	install -Dm644 completions/ferrisfetch.fish "$TERMUX_PREFIX/share/fish/vendor_completions.d/ferrisfetch.fish"
+	install -Dm755 target/${CARGO_TARGET_NAME}/release/ferrisfetch $TERMUX_PREFIX/bin/ferrisfetch
+	install -Dm644 completions/ferrisfetch.bash $TERMUX_PREFIX/share/bash-completion/completions/ferrisfetch
+	install -Dm644 completions/_ferrisfetch $TERMUX_PREFIX/share/zsh/site-functions/_ferrisfetch
+	install -Dm644 completions/ferrisfetch.fish $TERMUX_PREFIX/share/fish/vendor_completions.d/ferrisfetch.fish
 }
